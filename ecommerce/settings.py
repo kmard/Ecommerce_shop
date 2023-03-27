c7e167092ae8bd2232b850dbf5bef0ba4ba7b04d
KM <tsnok777@gmail.com>
2023-03-27 10:51:39 +0300

    Configuring Django for image uploads

--- ecommerce/settings.py
+++ ecommerce/settings.py
@@ -121,5 +121,9 @@ USE_TZ = True
 STATIC_URL = '/static/'
 STATICFILES_DIRS = [BASE_DIR/'static']
 
+MEDIA_URL = '/media/'
+MEDIA_ROOT = BASE_DIR/'static/media'
+
+
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
