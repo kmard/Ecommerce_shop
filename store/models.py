e65c296ed72bc136ca3d2170ec6fc67dcdf64edf
KM <tsnok777@gmail.com>
2023-03-27 11:47:55 +0300

    Basic - Django admin configuration

--- store/models.py
+++ store/models.py
@@ -15,7 +15,7 @@ class Category(models.Model):
 
 class Product(models.Model):
     title = models.CharField(max_length=250)
-    brand = models.CharField(max_length=250,default='un-barnded')
+    brand = models.CharField(max_length=250,default='un-branded')
     description = models.TextField(blank=True)
     slug = models.SlugField(max_length=255)
     price = models.DecimalField(max_digits=4, decimal_places=2)
@@ -25,4 +25,4 @@ class Product(models.Model):
         verbose_name_plural = 'products'
 
     def __str__(self):
-        return self.name
+        return self.title
