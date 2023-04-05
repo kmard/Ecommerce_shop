5a136991377a0448fa09a50fe43e45c2e0ccbe2b
KM <tsnok777@gmail.com>
2023-04-05 20:54:02 +0300

    Creating the singular product page

--- store/urls.py
+++ store/urls.py
@@ -7,4 +7,5 @@ from . import views
 urlpatterns = [
     # path('admin/', admin.site.urls),
     path('',views.store,name='store'),
+    path('product/<slug:slug>/',views.product_info,name='product-info'),
 ]
--- store/views.py
+++ store/views.py
@@ -1,5 +1,6 @@
 from django.shortcuts import render
 from .models import Category,Product
+from django.shortcuts import get_object_or_404
 
 
 # Create your views here.
@@ -13,4 +14,18 @@ def categories(request):
     all_categories = Category.objects.all()
 
     return {'all_categories':all_categories,}
+
+def product_info(request,slug):
+
+    product = get_object_or_404(Product,slug=slug)
+
+    context = {'product':product}
+
+    return render(request, 'store/product-info.html',context)
+
+
+
+
+
+
 
