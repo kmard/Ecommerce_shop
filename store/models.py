ac6399e86b55ee5011dc39a152533ef40bbe4f5e
KM <tsnok777@gmail.com>
2023-04-06 20:25:43 +0300

    Creating the singular product page

--- store/models.py
+++ store/models.py
@@ -1,4 +1,5 @@
 from django.db import models
+from django.urls import reverse
 
 # Create your models here.
 
@@ -11,6 +12,9 @@ class Category(models.Model):
 
     def __str__(self):
         return self.name
+
+    # def get_absolute_url(self):
+    #     return reverse('list-info',args=[self.slug])
 
 
 class Product(models.Model):
@@ -28,3 +32,6 @@ class Product(models.Model):
 
     def __str__(self):
         return self.title
+
+    def get_absolute_url(self):
+        return reverse('product-info',args=[self.slug])
