7341b1ad79bd0ffe4a62e457beac34c8309bc566
KM <tsnok777@gmail.com>
2023-04-09 12:28:50 +0300

    Create a Django app - (cart)

--- ecommerce/settings.py
+++ ecommerce/settings.py
@@ -37,7 +37,11 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'store',
+
+    'store', # Django app
+
+    'cart',  #Django app
+
 ]
 
 MIDDLEWARE = [
