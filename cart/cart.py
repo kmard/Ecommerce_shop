800ab40ee46504e4e83c6df6641b0e5086015a81
KM <tsnok777@gmail.com>
2023-04-25 19:07:07 +0300

    Delete functionality shopping cart

--- cart/cart.py
+++ cart/cart.py
@@ -58,6 +58,15 @@ class Cart():
 
         return sum(Decimal(item['price'])*item['qty'] for item in self.cart.values())
 
+    def delete(self,product):
+
+        product_id = str(product)
+
+        if product_id in self.cart:
+
+            del self.cart[product_id]
+
+        self.session.modified = True
 
 
 
