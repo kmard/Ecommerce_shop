d10dd8a48b0532dbdbfff34d6cb7eb0ae6b4d08f
KM <tsnok777@gmail.com>
2023-05-13 11:18:44 +0300

    8. Email verification - basic urls and views

--- account/urls.py
+++ account/urls.py
@@ -6,11 +6,16 @@ from . import views
 urlpatterns = [
 
     path('register',views.register,name='register'),
-    #
-    # path('add/',views.cart_add,name='cart-add'),
-    #
-    # path('delete/',views.cart_delete,name='cart-delete'),
-    #
-    # path('update/',views.cart_update,name='cart-update'),
+
+    #email verification URL's
+
+    path('email-verifiacation',views.email_verification,name='email-verifiacation'),
+
+    path('email-verifiacation-sent',views.email_verification_sent,name='email-verifiacation-sent'),
+
+    path('email-verifiacation-success', views.email_verification_success, name='email-verifiacation-success'),
+
+    path('email-verifiacation-failed', views.email_verification_failed, name='email-verifiacation-failed'),
+
 
 ]
