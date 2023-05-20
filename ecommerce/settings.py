e54621c9b6e24f4b07bfd4562eadf36201309545
KM <tsnok777@gmail.com>
2023-05-20 11:45:19 +0300

    13. Setup the email backend

--- ecommerce/settings.py
+++ ecommerce/settings.py
@@ -142,4 +142,14 @@ MEDIA_ROOT = BASE_DIR/'static/media'
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
-EMAIL_BACKEND = 'django.core.mail.backends.console.EmailBackend'
+# email configuration setting
+
+# EMAIL_BACKEND = 'django.core.mail.backends.console.EmailBackend'
+
+EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
+EMAIL_HOST = 'smtp.gmail.com'
+EMAIL_PORT = '587'
+EMAIL_USE_TLS = 'True'
+
+EMAIL_HOST_USER = '' # - Enter GMAIL address
+EMAIL_HOST_PASSWORD = '' #your app password
