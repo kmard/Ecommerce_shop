2297bb7878ca716df8542ed0a91f6c1697be7050
KM <tsnok777@gmail.com>
2023-06-16 10:41:18 +0300

    2. Integrating the password reset templates

--- account/urls.py
+++ account/urls.py
@@ -33,16 +33,16 @@ urlpatterns = [
     #password management urls/views
 
     #1) Submit our email form
-    path('reset_password', auth_views.PasswordResetView.as_view(), name='reset_password'),
+    path('reset_password', auth_views.PasswordResetView.as_view(template_name ='account/password/password-reset.html' ), name='reset_password'),
 
     #2)Success message stating that a password reset email was sent
-    path('reset_password_sent', auth_views.PasswordResetDoneView.as_view(), name='password_reset_done'),
+    path('reset_password_sent', auth_views.PasswordResetDoneView.as_view(template_name ='account/password/password-reset-sent.html'), name='password_reset_done'),
 
     #3) Password reset link
-    path('reset/<uidb64>/<token>/',auth_views.PasswordResetConfirmView.as_view(),name='password_reset_confirm'),
+    path('reset/<uidb64>/<token>/',auth_views.PasswordResetConfirmView.as_view(template_name ='account/password/password-reset-form.html'),name='password_reset_confirm'),
 
     #4)Success message starting that our password was reset
-    path('reset_password_complete',auth_views.PasswordResetCompleteView.as_view(),name='password_reset_complete'),
+    path('reset_password_complete',auth_views.PasswordResetCompleteView.as_view(template_name ='account/password/password-reset-complete.html'),name='password_reset_complete'),
 
 
 
