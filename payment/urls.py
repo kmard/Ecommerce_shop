30ca9f0bbcf5e9b629d95db1a9b15809012657e7
KM <tsnok777@gmail.com>
2023-07-02 10:48:45 +0300

    Build the checkout template

--- payment/urls.py
+++ payment/urls.py
@@ -6,10 +6,10 @@ from . import views
 
 urlpatterns = [
 
+    path('checkout',views.checkout,name='checkout'),
 
     path('payment-success',views.payment_success,name='payment-success'),
 
     path('payment-failed',views.payment_failed,name='payment-failed'),
-
 
 ]
--- payment/views.py
+++ payment/views.py
@@ -2,6 +2,10 @@ from django.shortcuts import render
 
 # Create your views here.
 
+def checkout(request):
+    return render(request,'payment/checkout.html')
+
+
 def payment_success(request):
 
     return render(request,'payment/payment-success.html')
