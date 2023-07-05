9633ccd56c73060fe1282662b5b99f5b1d5f1e06
KM <tsnok777@gmail.com>
2023-07-05 10:22:21 +0300

    Build the order and order item model

--- payment/models.py
+++ payment/models.py
@@ -1,5 +1,6 @@
 from django.db import models
 from django.contrib.auth.models import User
+from store.models import Product
 
 # Create your models here.
 
@@ -37,6 +38,43 @@ class ShippingAddress(models.Model):
         return 'Shipping Address - '+str(self.id)
 
 
+class Order(models.Model):
+
+    full_name = models.CharField(max_length=300)
+
+    email = models.EmailField(max_length=255)
+
+    shipping_address = models.TextField(max_length=500)
+
+    amount_paid = models.DecimalField(max_digits=8, decimal_places=2)
+
+    date_ordered = models.DateTimeField(auto_now=True)
+
+    #FK
+    user = models.ForeignKey(User, on_delete=models.CASCADE, null=True, blank=True)
+
+    def __str__(self):
+
+        return 'Order - #'+str(self.id)
+
+
+
+class OrderItems(models.Model):
+
+    #FK
+    order = models.ForeignKey(Order,on_delete=models.CASCADE,null=True)
+
+    product = models.ForeignKey(Product,on_delete=models.CASCADE,null=True)
+
+    quantity = models.PositiveIntegerField(default=1)
+
+    price = models.DecimalField(max_digits=8, decimal_places=2)
+
+    #FK
+    user = models.ForeignKey(User, on_delete=models.CASCADE, null=True, blank=True)
+
+    def __str__(self):
+        return 'Order Item - #' + str(self.id)
 
 
 
