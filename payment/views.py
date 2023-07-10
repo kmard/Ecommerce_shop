a54121c871733676fcc4019bd795f58812f868ac
KM <tsnok777@gmail.com>
2023-07-10 09:36:29 +0300

    AJAX redirection + Clear the shopping cart

--- payment/views.py
+++ payment/views.py
@@ -99,6 +99,12 @@ def complete_order(request):
 
 def payment_success(request):
 
+    #clear shopping cart
+    for key in list(request.session.keys()):
+        if key == 'session_key':
+
+            del request.session[key]
+
     return render(request,'payment/payment-success.html')
 
 def payment_failed(request):
